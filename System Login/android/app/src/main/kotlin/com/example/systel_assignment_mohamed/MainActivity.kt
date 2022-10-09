9106b1a0a6aebb847a1ade1796a1dd58ba82acbb
xinako <11191038@student.itk.ac.id>
2022-10-10 01:42:57 +0800

    Memperbaiki sedikit tampilan

--- System Login/android/app/src/main/kotlin/com/example/systel_assignment_mohamed/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.systel_assignment_mohamed
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- System Login/lib/consts/custom_enums.dart
+++
@@ -1,2 +0,0 @@
-
-enum Authenticated { authenticated, notAuthenticated }
